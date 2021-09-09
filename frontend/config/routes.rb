d97cc36c5d323c976855b618a18f0743116ec1bd
Mark Triggs <mark@dishevelled.net>
2021-09-09 14:28:32 +1000

    Interface for editing ARKs as the admin user

--- frontend/config/routes.rb
+++ frontend/config/routes.rb
@@ -282,6 +282,8 @@ ArchivesSpace::Application.routes.draw do
     match 'oai_config/edit'   => 'oai_config#edit',   :via => [:get]
     match 'oai_config/update' => 'oai_config#update', :via => [:post]
 
+    match 'ark_update' => 'ark_update#update', :via => [:post]
+
     # resources :custom_report_templates
     # match('custom_report_templates/:id/delete' => 'custom_report_templates#delete', :via => [:post])
     # match('custom_report_templates/:id' => 'custom_report_templates#update', :via => [:post])
