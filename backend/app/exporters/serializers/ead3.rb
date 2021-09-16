da5f7c7fa62c0b80d44f34f7e528f8b8da9685c5
Payten Giles <payten.giles@gmail.com>
2021-09-16 10:29:18 +1000

    EAD3 ref @actuate value onload should have little 'l'

--- backend/app/exporters/serializers/ead3.rb
+++ backend/app/exporters/serializers/ead3.rb
@@ -1198,7 +1198,7 @@ class EAD3Serializer < EADSerializer
       "type" => "ark",
     }) {
       xml.ref ({"href" => data.ark_name.fetch('current'),
-                "actuate" => "onLoad",
+                "actuate" => "onload",
                 "show" => "new",
       }) { xml.text 'Archival Resource Key' }
     }
@@ -1208,7 +1208,7 @@ class EAD3Serializer < EADSerializer
         "type" => "ark-superseded",
       }) {
         xml.ref ({"href" => old_ark_url,
-                  "actuate" => "onLoad",
+                  "actuate" => "onload",
                   "show" => "new",
         }) { xml.text 'Previous Archival Resource Key' }
       }
